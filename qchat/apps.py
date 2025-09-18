967b21d1f8e2a83633e8a90a5e07b1f929db7557
Aravind R Pillai <aravindrpillai1992@gmail.com>
2025-09-18 12:06:55 -0400

    apps

--- qchat/apps.py
+++ qchat/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 class QchatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-  
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qchat"
